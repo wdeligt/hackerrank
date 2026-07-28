570cb1b2fc02bfb4cc6bc190644f7722db0bcda2
Wouter de Ligt <150262066+wdeligt@users.noreply.github.com>
2026-07-28 17:11:45 +0200

    ✅ Find the Runner-Up Score! | Medium | Python | Runtime: N/A | Memory: N/A | 2026-07-28

--- hackerrank/medium/find-second-maximum-number-in-a-list/solution.py
+++ hackerrank/medium/find-second-maximum-number-in-a-list/solution.py
@@ -1,8 +1,4 @@
 if __name__ == '__main__':
-    X = int(input())
-    Y = int(input())
-    Z = int(input())
     n = int(input())
-    
-    out = [[x, y, z] for x in range(X+1) for y in range(Y+1) for z in range(Z+1) if x + y + z != n]
-    print(out)
+    arr = map(int, input().split())
+    print(sorted(set(arr))[-2])
