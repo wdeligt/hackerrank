1fe588bb62aea5195fb5a56f3b8cc554c2d46e85
Wouter de Ligt <150262066+wdeligt@users.noreply.github.com>
2026-07-28 18:07:25 +0200

    ✅ Finding the percentage | Medium | Python | Runtime: N/A | Memory: N/A | 2026-07-28

---
+++ hackerrank/medium/py-collections-deque/solution.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    sum = 0 
+    l = len(student_marks[query_name])
+    for num in student_marks[query_name]:
+        sum += num
+    average = round(sum / l,2 )
+    print(f"{average:.2f}")
+        
